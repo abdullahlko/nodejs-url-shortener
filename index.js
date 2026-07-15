9d07e3a5fedcb50c5f2d2f090fac0c44957e3cde
Abdullah Ansari <abdullahlko789@gmail.com>
2026-07-15 09:47:01 +0530

    Handle JSON requests and add URL creation endpoint

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const app = express();
 
 const PORT = 8001;
 
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello from server");
+});
+
+app.post("/url", (req, res) => {
+    console.log(req.body);
+    res.send("Received");
+});
+
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
