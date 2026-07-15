4d49398af06168583fbe30750b8a6d249320ce6c
Abdullah Ansari <abdullahlko789@gmail.com>
2026-07-15 10:39:03 +0530

    Set up Mongoose connection and define URL schema and model

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-
 const PORT = 8001;
 
+// Connect to MongoDB
+async function connectToMongoDB(url) {
+    return mongoose.connect(url);
+}
+
+// URL Schema
+const urlSchema = new mongoose.Schema({
+    shortId: String,
+    redirectURL: String,
+    visitHistory: [
+        {
+            timestamp: {
+                type: Number,
+            },
+        },
+    ],
+});
+
+// URL Model
+const URL = mongoose.model("URL", urlSchema);
+
+// Middleware
 app.use(express.json());
 
-app.get('/', (req, res) => {
+// Routes
+app.get("/", (req, res) => {
     res.send("Hello from server");
 });
 
@@ -15,6 +38,7 @@ app.post("/url", (req, res) => {
     res.send("Received");
 });
 
+// Start Server
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
